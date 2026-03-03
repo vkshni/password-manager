3af72f8658412ab3958ec1e7e408d1fc206bf514
vkshni <vksdrive24@gmail.com>
2026-03-03 21:16:35 +0530

    vault_meta, vault_data, attempts file

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+import json
 from os import mkdir
+
+from entity import Credential
 
 BASE_DIR = Path(__file__).parent
 
@@ -21,18 +24,94 @@ class JSONFile:
                 pass
         return path
 
+    def read_all(self):
+
+        with open(self.file_path, "r") as f:
+
+            data = json.load(f)
+            return data
+
+    def write_all(self, data):
+
+        with open(self.file_path, "w") as f:
+
+            json.dump(data, f, indent=4)
+
 
 class VaultMeta:
 
     def __init__(self):
-        pass
+        self.json_handler = JSONFile("vault_meta.json")
+        self.initialize()
+
+    def initialize(self):
+
+        try:
+            self.json_handler.read_all()
+        except:
+            data = {"master_password_hash": None}
+            self.json_handler.write_all(data)
+
+
+class VaultData:
+
+    def __init__(self):
+        self.json_handler = JSONFile("vault_data.json")
+        self.initialize()
+
+    def initialize(self):
+
+        try:
+            self.json_handler.read_all()
+        except:
+            self.json_handler.write_all([])
+
+    def add_data(self, credential: Credential):
+
+        credential_dict = credential.to_dict()
+
+        data = self.json_handler.read_all()
+
+        data.append(credential_dict)
+
+        self.json_handler.write_all(data)
 
 
 class Attempts:
 
     def __init__(self):
         self.json_handler = JSONFile("attempts.json")
+        self.initialize()
+
+    def initialize(self):
+
+        try:
+            self.json_handler.read_all()
+        except:
+            data = {"failed_count": 0, "locked_until": 0}
+            self.json_handler.write_all(data)
+
+    def reset_attempts(self):
+
+        data = {"failed_count": 0, "locked_until": 0}
+        self.json_handler.write_all(data)
+
+    def update(self, failed_count, locked_until):
+
+        data = self.json_handler.read_all()
+        data["failed_count"] = failed_count
+        data["locked_until"] = locked_until
+
+        self.json_handler.write_all(data)
 
 
 if __name__ == "__main__":
-    pass
+    attempts = Attempts()
+    vd = VaultData()
+    vm = VaultMeta()
+
+    # attempts.update(1, 2)
+    # attempts.reset_attempts()
+
+    c = Credential("instagram", "vksahani", "sdfkldl")
+    vd.add_data(c)
