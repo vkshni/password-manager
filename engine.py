241f3849c97d27185d3f3e35e230e499a0135485
vkshni <vksdrive24@gmail.com>
2026-03-04 21:55:42 +0530

    add method

--- engine.py
+++ engine.py
@@ -1,10 +1,23 @@
+from db import VaultData
+from entity import Credential
+
+import hashlib
+
+
 # Main Vault
-
-
 class Vault:
 
     def __init__(self):
-        pass
+        self.vd = VaultData()
 
-    def add_credential(self, service, username, password):
-        pass
+    def add_credential(self, service_name, username, password):
+
+        credential_obj = Credential(service_name, username, password)
+
+        self.vd.add_data(credential_obj)
+
+
+if __name__ == "__main__":
+
+    v = Vault()
+    # v.add_credential("GitHub", "vkshnii", "vks_GIT")
